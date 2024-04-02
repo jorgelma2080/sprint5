7061e9fd447f655f8e9b26411e09e72a8fb9f45d
jorgelma2080 <jorgelma.2080@gmail.com>
2024-04-02 12:12:13 -0500

    modifies

--- app.py
+++ app.py
@@ -1,28 +1,64 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
+import plotly.graph_objects as go
 
 car_data = pd.read_csv('notebooks/vehicles_us.csv')
 st.header('US Vehicles')
-hist_button = st.button('Histogram build')
-disp_button = st.button('Scatter graphich build')
+st.write('This is a demo of a data science web application')
 
-if hist_button:
-    '''
-    Construir boton que muestre el histograma
-    '''
-    st.write('Build a histogram of the Data')
-    fig = px.histogram(car_data, x = 'odometer')
-    st.plotly_chart(fig, use_container_width=True)
+st.header('Data viewer')
+table_check= st.checkbox('Show ramdon 1000 vechicles' ,True)
 
-if disp_button:
-    '''
-    Construir boton que muestre el grafico de dispersion
-        
-    '''
-    st.write('Build scatter graphic')
-    scatter_fig = px.scatter(car_data, x="odometer", y="price") 
-    st.plotly_chart(scatter_fig,use_container_width=True)
+if table_check:
+  st.dataframe(car_data.sample(100))
+else:
+   st.dataframe(car_data)
 
+st.header('Us Vehicles by Manufacturer')
+
+#split the brand of the vehicle
+brand_list = [] #empty list for the table
+for brand in car_data['model']:
+    brand_name=brand.split()
+    brand_list.append(brand_name[0])
+car_data['brand']=brand_list
+
+figpx = px.histogram(car_data,
+                     x='brand',
+                     color='type'
+                     )
+
+st.plotly_chart(figpx,use_container_width=True)
+
+st.header('Condition vs Year')
+car_data['brand']=brand_list
+
+figpx = px.histogram(car_data,
+                     x='model_year',
+                     color='condition'
+                     )
+st.plotly_chart(figpx,use_container_width=True)
+
+                       
+'''
+vehicle_by_bran=car_data.
+fig = go.Figure()
+
+for col in cols:
+    figpx = px.scatter(df.assign(Plot=col),
+                       x=col,
+                       y="Score",
+                       size="Population",
+                       color="Continent",
+                       hover_name="Country/Region",
+                       hover_data=["Plot"],
+                       size_max=60,
+                       color_discrete_sequence=px.colors.qualitative.G10).update_traces(visible=False)
     
+    fig.add_traces(figpx.data)
+
+'''
+
+
 
